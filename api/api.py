95ba2245ff03acb2f492ec6357ae8f969926eda3
pparramen <pparra981@gmail.com>
2025-04-26 19:24:34 +0200

    estructura

--- api/api.py
+++ api/api.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine, text
+from common.data_layer import cargar_cities
 
 app = FastAPI()
 
-# Crear el engine de conexión a PostgreSQL
-engine = create_engine(
-    "postgresql+psycopg2://admin:loYo1HT25@10.10.76.244:6565/greenlake_data"
-)
-
+@app.get("/api/cities")
+def obtener_cities():
+    df = cargar_cities()
+    return df.to_dict(orient="records")
 
---
+++ common/data_layer.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+def cargar_cities():
+    engine = create_engine(
+        'postgresql+psycopg2://admin:loYo1HT25@10.10.76.244:6565/greenlake_data'
+    )
+
+    query = "SELECT * FROM cities LIMIT 300;"
+    df = pd.read_sql(query, engine)
+    
+    return df
