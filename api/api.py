647c269ceba7c2b049bbcd8c2e3ac64f589585ba
Pablo Parra <pparra981@gmail.com>
2025-04-27 06:16:52 +0200

    recomendador musica

--- api/api.py
+++ api/api.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from common.data_layer import cargar_cities
+from typing import Optional
+from common.data_layer import cargar_people_enriquecido
 
 app = FastAPI()
 
-@app.get("/api/cities")
-def obtener_cities():
-    df = cargar_cities()
+@app.get("/api/greenlake-eval/people")
+def obtener_people(skip: int = 0, limit: int = 100):
+    df = cargar_people_enriquecido(skip=skip, limit=limit)
     return df.to_dict(orient="records")
-
